37d72ad8586ffec69680839a1cf3622dd0ef039e
cherry-nft <109643299+cherry-nft@users.noreply.github.com>
2024-02-22 16:44:03 -0800

    555

--- app/api/frame/hyperframes.ts
+++ app/api/frame/hyperframes.ts
@@ -89,7 +89,7 @@ const frames: Record<string, HyperFrame> = {
       input: { text: 'Try again. What is the password?' },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame?frame=shack-bad-password`,
     }),
-    1: 'road',
+    1: 'cava',
     2: text => checkForCorrectText('guess', text) ? 'key' : 'shack-bad-password',
   },
   'key': {
